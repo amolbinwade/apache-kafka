52a4af9bba6cdaa2fcc403fd4e699ed086b9ff9e
amolbinwade <amolbinwade@gmail.com>
2024-09-01 11:37:12 +0530

    Formatted code

--- kafka-spring-streams-design-patterns/src/test/java/com/amcode/kafka/streams/patterns/LocalStateProcessingTest.java
+++ kafka-spring-streams-design-patterns/src/test/java/com/amcode/kafka/streams/patterns/LocalStateProcessingTest.java
@@ -11,7 +11,6 @@ import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.time.*;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
@@ -19,63 +18,84 @@ public class LocalStateProcessingTest {
 
     private LocalStateProcessing localStateProcessing;
     private Properties props = new Properties();
-    private String eventDate, getEventDate2;
+    private String eventDate;
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
         localStateProcessing = new LocalStateProcessing();
         localStateProcessing.setBOOTSTRAP_SERVER("test");
         localStateProcessing.setKAFKA_TOPIC("input_topic");
         localStateProcessing.setLOCAL_STATE_STATS_KAFKA_TOPIC("output_topic");
         props.putAll(localStateProcessing.getConfigs());
+        eventDate = "01-Sep-2024 11:11:11";
     }
 
     @Test
-    public void testLocalStateProcessing(){
+    public void testLocalStateProcessing() {
         StreamsBuilder streamsBuilder = new StreamsBuilder();
         localStateProcessing.localStateProcessingKStream(streamsBuilder);
         Topology topology = streamsBuilder.build();
 
-        try(TopologyTestDriver topologyTestDriver = new TopologyTestDriver(topology, props)){
+        try (TopologyTestDriver topologyTestDriver = new TopologyTestDriver(topology, props)) {
             TestInputTopic<String, StockTickerRecord> inputTopic = topologyTestDriver
-                    .createInputTopic("input_topic", new StringSerializer(), new JsonSerializer<StockTickerRecord>());
+                    .createInputTopic("input_topic", new StringSerializer(),
+                            new JsonSerializer<StockTickerRecord>());
             TestOutputTopic<String, StockStats> outputTopic = topologyTestDriver
-                    .createOutputTopic("output_topic", new StringDeserializer(), new JsonDeserializer<StockStats>(StockStats.class));
-
-            LocalDate localDate = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
-                    LocalDateTime localDateTime = LocalDateTime.of(localDate.getYear(),
-                    localDate.getMonthValue(),
-                    localDate.getDayOfMonth(), 12, 0, 18);
-            Instant instant = localDateTime.toInstant(ZoneOffset.UTC);
-
-            inputTopic.pipeInput("ITC", new StockTickerRecord(eventDate, "ITC", 101.11f), instant);
-
-            inputTopic.pipeInput("ITC", new StockTickerRecord(eventDate, "ITC", 102.11f), instant.plusSeconds(1));
-
-            inputTopic.pipeInput("ITC", new StockTickerRecord(eventDate, "ITC", 103.11f), instant.plusSeconds(2));
-
-            inputTopic.pipeInput("ITC", new StockTickerRecord(eventDate, "ITC", 100.11f), instant.plusSeconds(3));
-
-            inputTopic.pipeInput("ITC", new StockTickerRecord(eventDate, "ITC", 107.11f), instant.plusSeconds(4));
-
-
-            inputTopic.pipeInput("ITC", new StockTickerRecord(eventDate, "ITC", 99.11f), instant.plusSeconds(5));
-
-            inputTopic.pipeInput("ITC", new StockTickerRecord(eventDate, "ITC", 98.11f), instant.plusSeconds(6));
-
-            inputTopic.pipeInput("ITC", new StockTickerRecord(eventDate, "ITC", 97.11f), instant.plusSeconds(7));
-
-            inputTopic.pipeInput("ITC", new StockTickerRecord(eventDate, "ITC", 88.11f), instant.plusSeconds(8));
-
-            inputTopic.pipeInput("ITC", new StockTickerRecord(eventDate, "ITC", 99.21f), instant.plusSeconds(9));
-
-
+                    .createOutputTopic("output_topic", new StringDeserializer(),
+                            new JsonDeserializer<StockStats>(StockStats.class));
+            populateInputTopic(inputTopic);
             List list = outputTopic.readKeyValuesToList();
             list.forEach(System.out::println);
 
         }
+    }
 
+    private void populateInputTopic(TestInputTopic<String, StockTickerRecord> inputTopic) {
+        LocalDate localDate = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+        LocalDateTime localDateTime = LocalDateTime.of(localDate.getYear(),
+                localDate.getMonthValue(),
+                localDate.getDayOfMonth(), 12, 0, 18);
+        Instant instant = localDateTime.toInstant(ZoneOffset.UTC);
 
+        inputTopic.pipeInput("ITC",
+                new StockTickerRecord(eventDate, "ITC", 101.11f),
+                instant);
+
+        inputTopic.pipeInput("ITC",
+                new StockTickerRecord(eventDate, "ITC", 102.11f),
+                instant.plusSeconds(1));
+
+        inputTopic.pipeInput("ITC",
+                new StockTickerRecord(eventDate, "ITC", 103.11f),
+                instant.plusSeconds(2));
+
+        inputTopic.pipeInput("ITC",
+                new StockTickerRecord(eventDate, "ITC", 100.11f),
+                instant.plusSeconds(3));
+
+        inputTopic.pipeInput("ITC",
+                new StockTickerRecord(eventDate, "ITC", 107.11f),
+                instant.plusSeconds(4));
+
+        inputTopic.pipeInput("ITC",
+                new StockTickerRecord(eventDate, "ITC", 99.11f),
+                instant.plusSeconds(5));
+
+        inputTopic.pipeInput("ITC",
+                new StockTickerRecord(eventDate, "ITC", 98.11f),
+                instant.plusSeconds(6));
+
+        inputTopic.pipeInput("ITC",
+                new StockTickerRecord(eventDate, "ITC", 97.11f),
+                instant.plusSeconds(7));
+
+        inputTopic.pipeInput("ITC",
+                new StockTickerRecord(eventDate, "ITC", 88.11f),
+                instant.plusSeconds(8));
+
+        inputTopic.pipeInput("ITC",
+                new StockTickerRecord(eventDate, "ITC", 99.21f),
+                instant.plusSeconds(9));
     }
 
 
